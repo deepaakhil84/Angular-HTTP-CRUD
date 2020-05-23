2b01dfbaebc0342749dfd1827b6b93d099638c6c
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-05-23 19:04:37 +0100

    Rename project to all lowercase

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-Http-CRUD'),
+      dir: require('path').join(__dirname, './coverage/angular-http-crud'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
