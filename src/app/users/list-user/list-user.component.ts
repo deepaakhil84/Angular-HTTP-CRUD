a4bcb2fe0ae73b550000304ddf0f3b8e1ad6f711
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-05-23 18:24:48 +0100

    add a command on ts file to display the list of data into the frame work

--- src/app/users/list-user/list-user.component.ts
+++ src/app/users/list-user/list-user.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { User } from "../../model/user";
+import { JsonPlaceholderService } from "../../Services/json-placeholder.service"
+import { Observable } from 'rxjs';
+import "../styles/styles.scss"
+
 
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.scss']
 })
-export class ListUserComponent implements OnInit {
+export class ListUserComponent {
+  constructor(private jsonplaceholder: JsonPlaceholderService) { }
+  users: Observable<User[]>
+  ngOnInit() {
+    this.users = this.jsonplaceholder.getdata()
+    console.log("ng on it", this.users);
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  getDataFromApi() {
+
+  }
+
+
 }
+
+
