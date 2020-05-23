fc2aa88d8d2c2ec40e3b4333210d260045c7bc99
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-05-23 21:18:01 +0100

    created a new component to display the dtails of the user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListUserComponent } from './users/list-user/list-user.component';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListUserComponent
+    ListUserComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
